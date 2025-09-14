1f2758966d6fbc92182d62e279cf692ac0a1cd64
Baransel <baranselklnc@gmail.com>
2025-09-14 03:33:53 +0300

    API temel URL'si 'localhost' yerine '127.0.0.1' olarak güncellendi.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 /// App-wide constants
 class AppConstants {
   // API
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://127.0.0.1:8000';
   static const String apiVersion = '/api/v1';
   
   // Storage
